e7fea887bac5c03d12c4551c56dc045e35b9cb1c
DANHUI JIA <danhuijia@DANHUIs-MacBook-Air-2.local>
2023-05-08 12:41:32 -0700

    revert config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,6 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-      },
-    },
-    `gatsby-transformer-csv`,
+
   ],
 }
