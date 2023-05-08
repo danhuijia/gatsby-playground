2fdedc8c2ae2abb5bffa6a6da15f4f5c350a8d33
DANHUI JIA <danhuijia@DANHUIs-MacBook-Air-2.local>
2023-05-08 01:19:41 -0700

    play ground ready.

---
+++ src/components/Problem.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState, useReducer } from "react"
+import { container } from './problem.module.css'
+const _ = require('lodash')
+
+
+
+const Problem = (props) => {
+    console.log('%cSolving starts here ...', "color: green")
+
+
+
+
+    useEffect(() => {
+
+    }, [])
+
+    return <div className={container}>
+
+
+
+    </div>
+}
+export default Problem;
